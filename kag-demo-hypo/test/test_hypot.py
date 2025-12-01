c608c63a6df94c25e52379d4ec9b82302819f345
Alexis Gourdol <alexis.gourdol@gmail.com>
2025-12-01 17:54:41 +0100

    fix format with ruff

--- kag-demo-hypo/test/test_hypot.py
+++ kag-demo-hypo/test/test_hypot.py
@@ -5,10 +5,11 @@ from conftest import raw_
 
 import kag
 
+
 def hypot_df_generator():
     df = raw_()
     cols = []
-    for col in ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q8', 'Q9']:
+    for col in ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q8", "Q9"]:
         cols.append(column(col, elements=strategies.sampled_from(df[col].unique())))
     return data_frames(columns=cols)
 
@@ -19,4 +20,3 @@ def test_countries(gen_df):
         return
     kag_ = kag.tweak_kag(gen_df)
     assert len(kag_.Country.unique()) <= 4
-
